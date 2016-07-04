949812b1f9ca7b5a74ef703de7ce1289e6e28050
Kudusch <kudusch@gmail.com>
2016-07-05 00:48:56 +0200

    Change to soft tabs

--- controller.py
+++ controller.py
@@ -19,25 +19,25 @@ values = re.findall(re.compile(ur'^Werte: (.*)$', re.MULTILINE) , options)
 
 files = []
 for option in var:
-	files.append(generateTimeSeries(start, end, option, values[var.index(option)], delimiter, maxSeconds, data))
+    files.append(generateTimeSeries(start, end, option, values[var.index(option)], delimiter, maxSeconds, data))
 
 for f in files:
-	current = csv.reader(open(f, 'r', ), delimiter=delimiter)
-	if files.index(f) > 0:
-		writer = csv.writer(open('temp.csv', 'w'), delimiter=delimiter)
-		reader = csv.reader(open('merged.csv', 'r'), delimiter=delimiter)
-	else:
-		writer = csv.writer(open('merged.csv', 'w'), delimiter=delimiter)
-		reader = csv.reader(open('columns.csv', 'r'), delimiter=delimiter)
-	for row in current:
-		next = reader.next()
-		next.extend(row)
-		writer.writerow(next)
-	try:
-		os.rename('temp.csv', 'merged.csv')
-	except:
-		pass
-	os.remove(f)
+    current = csv.reader(open(f, 'r', ), delimiter=delimiter)
+    if files.index(f) > 0:
+        writer = csv.writer(open('temp.csv', 'w'), delimiter=delimiter)
+        reader = csv.reader(open('merged.csv', 'r'), delimiter=delimiter)
+    else:
+        writer = csv.writer(open('merged.csv', 'w'), delimiter=delimiter)
+        reader = csv.reader(open('columns.csv', 'r'), delimiter=delimiter)
+    for row in current:
+        next = reader.next()
+        next.extend(row)
+        writer.writerow(next)
+    try:
+        os.rename('temp.csv', 'merged.csv')
+    except:
+        pass
+    os.remove(f)
 os.remove('columns.csv')
 now = datetime.datetime.today()
 fileName = now.strftime("%Y-%m-%d %H.%M.%S.csv")
--- utilities.py
+++ utilities.py
@@ -3,23 +3,23 @@
 import string
 
 def stampToSeconds(stamp):
-	stamp = stamp.split(':')
-	out = 0
-	for n in stamp:
-		out += int(n) * (60**list(reversed(stamp)).index(n))
-	return out
-	
+    stamp = stamp.split(':')
+    out = 0
+    for n in stamp:
+        out += int(n) * (60**list(reversed(stamp)).index(n))
+    return out
+    
 def secondsToStamp(seconds):
-	h = seconds / 3600
-	m = (seconds % 3600) / 60
-	s = (seconds % 3600) % 60
-	return ':'.join(['%02d' % h, '%02d' % m, '%02d' % s])
-	
+    h = seconds / 3600
+    m = (seconds % 3600) / 60
+    s = (seconds % 3600) % 60
+    return ':'.join(['%02d' % h, '%02d' % m, '%02d' % s])
+    
 def flatten(l, values):
-	d = dict((i, 0) for i in values)
-	for item in l:
-		d[item] += 1
-	out = []
-	for key in sorted(d.iterkeys()):
-		 out.append(d[key])
-	return out
+    d = dict((i, 0) for i in values)
+    for item in l:
+        d[item] += 1
+    out = []
+    for key in sorted(d.iterkeys()):
+         out.append(d[key])
+    return out
